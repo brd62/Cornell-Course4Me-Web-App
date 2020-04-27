b06f1e45e3eb79b08c0b6765a35c49d2a0fc95df
Julian Londono <jlond10@gmail.com>
2020-04-27 04:51:38 -0400

    Add autocomplete class input box

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,9 +2,17 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from app.irsystem.models.search import *
+import pickle
 
 project_name = "Course4me"
 net_id = "Brady Dicken (brd62), Micah Wallingford (mjw286), Sam Rosenthal (ser259), Julian Londono (jl3358)"
+
+classes_dict = pickle.load(open( "./class_roster_api_dict.pickle", "rb"))
+classes_list = set()
+for class_key in classes_dict.keys():
+	for class_name in classes_dict[class_key]["subject-number"]:
+		classes_list.add(class_name.upper())
+		
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -33,7 +41,9 @@ def search():
 		suggestions = []
 		output_message = ''
 
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, suggestions= suggestions)
+	return render_template('search.html', name=project_name, netid=net_id, 
+							output_message=output_message, data=data, suggestions= suggestions,
+							classes_list=classes_list)
 
 
 
