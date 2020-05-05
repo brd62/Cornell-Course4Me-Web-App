4d5093e9a742e35443781c3fe2edde92566d60e9
sam-rosenthal <ser259@cornell.edu>
2020-05-04 23:12:41 -0400

    Added toggle

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -35,8 +35,8 @@ def search():
 						request.args[key].split("-")[0] == "irrelevant"]
 					
 	original_query = ''
+	toggle_query = request.args.get('toggle_search')
 	keyword_query = request.args.get('keyword_search')
-	# professor_query = request.args.get('professor_search')
 	class_query = request.args.get('class_search')
 	suggestion = request.args.get('suggestion_search')
 	rocchio_update_query = request.args.get('rocchio_update_query')
@@ -48,34 +48,46 @@ def search():
 	if(suggestion):
 		keyword_query = suggestion
 
-	if keyword_query and not rocchio_update_query:
-		data = getKeywordResults(keyword_query, classLevel_query, semester_query, major_query)
-		suggestions = getSuggestions(keyword_query)
-		original_query = keyword_query
-		if len(data) > 0 :
-			output_message = "Results for \"" + keyword_query + "\"" 
-		else:
-			output_message = "No results found for \"" + keyword_query + "\""
+	print(toggle_query)
+	if keyword_query and not rocchio_update_query and toggle_query==None:
+		if class_query: 
+			data = []
+			suggestions = []
+			output_message = 'Cannot input class when keyword search is selected'
 
-		if classLevel_query != None and classLevel_query !="":
-			output_message += '\n' + "Class levels in the range [" + classLevel_query+"]"
-		if semester_query != None and semester_query !="":
-			output_message += '\n' + "Classes in the " + semester_query+" semester"
-		if major_query != None and major_query !="":
-			output_message += '\n' + "Classes in the " + major_query+" major"
+		else:
+			data = getKeywordResults(keyword_query, classLevel_query, semester_query, major_query)
+			suggestions = getSuggestions(keyword_query)
+			original_query = keyword_query
+			if len(data) > 0 :
+				output_message = "" 
+			else:
+				output_message = "No results found"
+
+			# if classLevel_query != None and classLevel_query !="":
+			# 	output_message += '\n' + "Class levels in the range [" + classLevel_query+"]"
+			# if semester_query != None and semester_query !="":
+			# 	output_message += '\n' + "Classes in the " + semester_query+" semester"
+			# if major_query != None and major_query !="":
+			# 	output_message += '\n' + "Classes in the " + major_query+" major"
 
 		
-	elif class_query and not rocchio_update_query:
-		data = getClassResults(class_query)
-		suggestions = []
-		original_query = class_query
-
-		if len(data) > 0 :
-			output_message = "Results for "+ class_query
+	elif class_query and not rocchio_update_query and toggle_query=="on":
+		if keyword_query: 
+			data = []
+			suggestions = []
+			output_message = 'Cannot input keyword when class search is selected'
 		else:
-			output_message = "No results found for \"" + keyword_query + "\""
+			data = getClassResults(class_query)
+			suggestions = []
+			original_query = class_query
+
+			if len(data) > 0 :
+				output_message = ""
+			else:
+				output_message = "No results found"
+		
 	elif rocchio_update_query:
-		
 		new_query = rocchio(rocchio_update_query, relevant_ids, irrelevant_ids)
 		
 		print(new_query)
@@ -83,17 +95,21 @@ def search():
 		data = getKeywordResults(new_query, classLevel_query=None, semester_query=None, major_query=None)
 		suggestions = getSuggestions(new_query)
 		if len(data) == 0 :
-			output_message = "No results found for \"" + keyword_query + "\"" + " after Rocchio Update"
+			output_message = "No results found after Rocchio Update"
 			original_query = keyword_query
 		else:
-			output_message = "Updated Rocchio Results for \"" + new_query + "\""
+			output_message = "Updated Rocchio Results:"
 			original_query = new_query
 	
 	
 	else:
 		data = []
 		suggestions = []
-		output_message = ''
+		if toggle_query==None:
+			output_message = 'Need to input a keyword'
+		else:
+			output_message = 'Need to input a class'
+
 
 	return render_template('search.html', name=project_name, netid=net_id,
 							output_message=output_message, data=data, suggestions= suggestions,
